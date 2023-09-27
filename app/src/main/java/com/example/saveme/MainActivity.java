b6936a5bef2c1942d1bbe78f23939da05b47dabf
wicho <luismahe2004@gmail.com>
2023-09-27 12:15:49 -0600

    Initial commit

---
+++ app/src/main/java/com/example/saveme/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.saveme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button B;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        B=(Button) findViewById(R.id.but);
+        B.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        Intent i=new Intent(this, MainActivity2.class);
+        startActivity(i);
+    }
+}
